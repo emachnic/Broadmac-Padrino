b7a299f534c9fe1d8628d5b40480acd617f7aa11
Evan Machnic <emachnic@gmail.com>
2010-09-09 12:07:43 -0400

    Added friendly urls

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title,
+            :presence => true, 
+            :uniqueness => true
+  validates :body,
+            :presence => true
 
+  def to_param
+    "#{id}-#{title.gsub(/[^a-z0-9]+/i, '-')}"
+  end
 end
